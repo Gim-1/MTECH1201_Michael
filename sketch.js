d755b2e2cb24a67fa0c0a8eded3759c3601fe02d
Gim-1 <michaelybes@gmail.com>
2026-02-10 19:08:41 -0500

    comit
    day one city drawing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+
 }
 
 function draw() {
   background(220);
+   fill('gray');
+  rect(0,400,500,100);
+   fill('white');
+  rect(0,450,50,10);
+ fill('white');
+  rect(100,450,50,10);
+  fill('white');
+  rect(200,450,50,10);
+  fill('white');
+  rect(300,450,50,10)
+  fill('white');
+  rect(400,450,50,10);
+  fill('green');
+  rect(0,400,500,10);
+fill('gray');
+rect(200,175,125,225);
+fill('brown');
+rect(20,310,10,100);
+
+stroke('black');
+strokeWeight(2);
+square(20,305,10,)
 }
