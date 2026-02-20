e2595d51667bff624fc80375706afeb016ec60a1
Gim-1 <michaelybes@gmail.com>
2026-02-19 20:25:24 -0500

    ss2 done

---
+++ sketch.js
@@ -0,0 +1,107 @@
+function setup() {
+  createCanvas(500, 500);
+
+}
+
+function draw() {
+  background(143, 247, 252);
+
+  textSize(24);
+
+  text("Michael Suero", 10,100);
+
+  fill(255,46,5)
+circle(485,25,100)
+
+   fill('gray');
+  rect(0,400,500,100);
+   fill('white');
+  rect(0,450,50,10);
+ fill('white');
+  rect(100,450,50,10);
+  fill('white');
+  rect(200,450,50,10);
+  fill('white');
+  rect(300,450,50,10)
+  fill('white');
+  rect(400,450,50,10);
+  fill('green');
+  rect(0,400,500,10);
+fill('gray');
+rect(200,175,125,225);
+fill(71, 215, 255);
+rect(220,200,20,30);
+rect(280,200,20,30);
+rect(220,250,20,30);
+rect(280,250,20,30);
+rect(220,300,20,30);
+rect(280,300,20,30)
+fill('black')
+rect(245,350,30,50,)
+fill('white')
+circle(270,375,10)
+
+
+fill('brown');
+rect(20,310,10,100);
+stroke('black');
+strokeWeight(2);
+fill('black');
+square(20,305,10,);
+fill('brown')
+rect(30,310,40,5)
+
+fill('brown');
+rect(170,310,10,100);
+fill('black');
+square(170,305,10,);
+fill('brown')
+rect(130,310,40,5)
+
+fill('brown');
+rect(340,310,10,100);
+fill('black');
+square(340,305,10,);
+fill('brown')
+rect(350,310,40,5)
+
+fill('brown');
+rect(490,310,10,100);
+fill('black');
+square(490,305,10,);
+fill('brown')
+rect(450,310,40,5)
+
+fill('brown');
+rect(60,370,10,40);
+fill('green');
+triangle(50,380,65,335,80,380);
+triangle(40,370,65,325,90,370);
+
+fill('brown');
+rect(130,370,10,40);
+fill('green');
+triangle(120,380,135,335,150,380);
+triangle(110,370,135,325,160,370);
+
+
+
+fill('brown');
+rect(380,370,10,40);
+fill('green');
+triangle(370,380,385,335,400,380);
+triangle(360,370,385,325,410,370);
+
+fill('brown');
+rect(450,370,10,40);
+fill('green');
+triangle(440,380,455,335,470,380);
+triangle(430,370,455,325,480,370);
+
+fill('red');
+rect(30,410,80,40);
+
+
+
+
+}
